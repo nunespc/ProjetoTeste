78b76e38540675279cdb946435bed0d65eb57dd9
Wanderson Nunes <87546860+nunespc@users.noreply.github.com>
2022-01-30 01:58:10 -0300

    Add files via upload

--- ProjetoTeste/src/com/nunespc/Projeto.java
+++ ProjetoTeste/src/com/nunespc/Projeto.java
@@ -1,6 +1,5 @@
 package com.nunespc;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /*
---
+++ ProjetoTeste/src/com/nunespc/Projeto2.java
@@ -0,0 +1,30 @@
+package com.nunespc;
+
+import java.util.Scanner;
+
+public class Projeto2 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Informe a quantidade de aluno: ");
+		int qtdAluno = entrada.nextInt();
+		
+		System.out.println("Informe a quantidade de nota: ");
+		int qtdNota = entrada.nextInt();
+		
+		double [][] notaDaTurma = new double[qtdAluno][qtdNota];
+		
+		double total = 0;
+		for(int a=0; a<notaDaTurma.length; a++) {
+			for (int n=0; n<notaDaTurma[a].length; n++) {
+				System.out.printf("Informe a nota "+(n+1)+" do aluno "+(a+1)+":", n, a);
+					notaDaTurma[a][n] = entrada.nextDouble();
+					total += notaDaTurma[a][n];
+				}
+			
+		}
+		double media = total / (qtdAluno * qtdNota);
+		System.out.println("Média da turma é "+media+"!");
+		entrada.close();
+	}
+
+}
